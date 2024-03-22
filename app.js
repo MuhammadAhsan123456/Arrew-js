3258fe118827c9c4e678f023473c553c4ee3a0b9
junaidhnhtechsolutions <junaid@hnhtechsolutions.com>
2024-03-22 18:31:34 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+
+
+// var student = ["Zaid" , "Haris" , "Abu Bakar" , "Hamza" , "Zaid" , "Haris" , "Abu Bakar" , "Hamza"] 
+// var i = student.lastIndexOf('Hamza')
+// console.log(i)  
+
+// var student = ["Zaid" , "Haris" , "Abu Bakar" , "Hamza"] 
+// var i = student.indexOf('Haris')
+// console.log(i)
+
+
+// var a = ["Zaid" , "Haris" , "Abu Bakar" , "Hamza"]
+// a[3] = "Abdul Salam"
+// console.log(a)
+
+
+// var student = ["Zaid" , "Haris" , "Abu Bakar" , "Hamza" , "Zaid" , "Haris" , "Abu Bakar" , "Hamza"] 
+// var a = student.length
+// console.log(a)
+
+
+// var student = ["Zaid" , "Haris" , "Abu Bakar" , "Hamza"] 
+// var last = student.length
+// var std = student[last-1]
+// console.log(std)
+
+
+
+// var student = ["Zaid","Haris","Zaid", "Abu Bakar","Hamza"] 
+// var b = student.splice(0,2,"Muhammad umar","Muhammad usman")
+// // console.log(b)
+// console.log(student)
+
+
+// var student = ["Zaid","Haris","Abu Bakar","Hamza"]
+// var i = student.indexOf("Hamza")
+// console.log(i)
+
+
+// var student = ["Zaid","Haris","Abu Bakar","Hamza"]
+// student[3] = "Abdul salam"
+// console.log(student)
+
+
+var student = ["Zaid","Haris","Zaid", "Abu Bakar","Hamza"]
+console.log(student.slice(2 , 4)) 
+
+
+
+
+
+
+
+
+
